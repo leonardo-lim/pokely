83c98114100243e60959cba03404cfe6b2eee761
leonardo-lim <leonardolim178@gmail.com>
2025-08-08 18:41:15 +0700

    feat: add home page pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 
 import type { Pokemon } from '@/types/pokemon-type';
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
-import { For, Grid, GridItem, HStack, Spinner, Text, VStack } from '@chakra-ui/react';
+import { useEffect, useRef, useState } from 'react';
+import { For, Grid, GridItem, HStack, IconButton, Spinner, Text, VStack } from '@chakra-ui/react';
+import { AngleLeft, AngleRight } from '@/lib/icon';
 import axiosInstance from '@/lib/axios-instance';
 import PokeCard from '@/components/PokeCard';
 
@@ -11,7 +12,10 @@ const LIMIT = 20;
 
 const Home: NextPage = () => {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    const [offset, setOffset] = useState(0);
     const [isLoading, setIsLoading] = useState(true);
+
+    const pokemonTotal = useRef(0);
 
     const getPokemonDetails = async (pokemonId: number) => {
         const response = await axiosInstance.get(`/pokemon/${pokemonId}`);
@@ -19,20 +23,35 @@ const Home: NextPage = () => {
     };
 
     const getPokemons = async () => {
-        const response = await axiosInstance.get(`/pokemon?offset=0&limit=${LIMIT}`);
+        setIsLoading(true);
+
+        const response = await axiosInstance.get(`/pokemon?offset=${offset}&limit=${LIMIT}`);
         const results = response.data.results as Pokemon[];
+        pokemonTotal.current = response.data.count;
 
         const pokemonDetails = await Promise.all(
-            results.map((_, idx) => getPokemonDetails(idx + 1))
+            results.map((_, idx) => getPokemonDetails(offset + idx + 1))
         );
 
         setPokemons(pokemonDetails);
         setIsLoading(false);
     };
 
+    const handlePreviousPokemons = () => {
+        if (offset > 0) {
+            setOffset(offset - LIMIT);
+        }
+    };
+
+    const handleNextPokemons = () => {
+        if (offset + LIMIT < pokemonTotal.current) {
+            setOffset(offset + LIMIT);
+        }
+    };
+
     useEffect(() => {
         getPokemons();
-    }, []);
+    }, [offset]);
 
     return (
         <VStack gap={4} align="start" px={[10, 10, 20, 20, 40]} py={[8, 8, 8, 8, 10]}>
@@ -43,26 +62,55 @@ const Home: NextPage = () => {
                     <Text>Catching wild Pokémon</Text>
                 </HStack>
             ) : (
-                <Grid
-                    gap={[6, 6, 6, 6, 8]}
-                    templateColumns={[
-                        'repeat(1, 1fr)',
-                        'repeat(2, 1fr)',
-                        'repeat(3, 1fr)',
-                        'repeat(4, 1fr)',
-                        'repeat(4, 1fr)',
-                        'repeat(5, 1fr)'
-                    ]}
-                    w="100%"
-                >
-                    <For each={pokemons}>
-                        {(pokemon, pokemonIdx) => (
-                            <GridItem key={pokemonIdx}>
-                                <PokeCard pokemon={pokemon} />
-                            </GridItem>
-                        )}
-                    </For>
-                </Grid>
+                <VStack gap={5} w="100%">
+                    <Grid
+                        gap={[6, 6, 6, 6, 8]}
+                        templateColumns={[
+                            'repeat(1, 1fr)',
+                            'repeat(2, 1fr)',
+                            'repeat(3, 1fr)',
+                            'repeat(4, 1fr)',
+                            'repeat(4, 1fr)',
+                            'repeat(5, 1fr)'
+                        ]}
+                        w="100%"
+                    >
+                        <For each={pokemons}>
+                            {(pokemon, pokemonIdx) => (
+                                <GridItem key={pokemonIdx}>
+                                    <PokeCard pokemon={pokemon} />
+                                </GridItem>
+                            )}
+                        </For>
+                    </Grid>
+                    <HStack>
+                        <IconButton
+                            variant="solid"
+                            borderRadius={12}
+                            onClick={handlePreviousPokemons}
+                            disabled={offset === 0}
+                            _icon={{
+                                w: ['16px', '16px', '18px'],
+                                h: ['16px', '16px', '18px']
+                            }}
+                        >
+                            {AngleLeft}
+                        </IconButton>
+                        <Text>{offset + 1} - {offset + LIMIT} of {pokemonTotal.current}</Text>
+                        <IconButton
+                            variant="solid"
+                            borderRadius={12}
+                            onClick={handleNextPokemons}
+                            disabled={offset + LIMIT >= pokemonTotal.current}
+                            _icon={{
+                                w: ['16px', '16px', '18px'],
+                                h: ['16px', '16px', '18px']
+                            }}
+                        >
+                            {AngleRight}
+                        </IconButton>
+                    </HStack>
+                </VStack>
             )}
         </VStack>
     );
