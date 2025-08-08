3d37781eef656a0147df497b15008ac8e6b9d836
leonardo-lim <leonardolim178@gmail.com>
2025-08-08 16:55:24 +0700

    feat: add pokemon type colors

---
+++ src/lib/type-color.ts
@@ -0,0 +1,24 @@
+const typeColors: Record<string, string> = {
+    grass: 'green.400',
+    poison: 'purple.500',
+    fire: 'red.400',
+    water: 'blue.400',
+    bug: 'green.600',
+    normal: 'gray.400',
+    flying: 'cyan.400',
+    electric: 'yellow.400',
+    ground: 'orange.400',
+    fairy: 'pink.400',
+    fighting: 'red.600',
+    psychic: 'purple.300',
+    rock: 'gray.600',
+    ice: 'cyan.200',
+    ghost: 'purple.700',
+    dragon: 'purple.800',
+    dark: 'gray.700',
+    steel: 'gray.500',
+    shadow: 'blackAlpha.500',
+    unknown: 'gray.300'
+};
+
+export { typeColors };
